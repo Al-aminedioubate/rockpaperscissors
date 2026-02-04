6cd57bd1089b7d76a640564b31b887bc2f5c9113
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-04 00:24:20 -0500

    declaration et debut js

---
+++ index.js
@@ -0,0 +1,7 @@
+//Declaration et initialisation de nos variales
+let rock = document.getElementById("rockBtn");
+let paper = document.getElementById("paperBtn");
+let scissors = document.getElementById("scissorsBtn");
+
+let randomValue;
+
